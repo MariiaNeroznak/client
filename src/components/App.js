f5f24dfc476314f32a787069e8581974d0f57057
Mariia <mariia.neroznak@visma.com>
2022-05-20 14:46:38 +0300

    Add routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>Page one</div>;
+};
+const PageTwo = () => {
+  return (
+    <div>
+      Page two <br />
+      <button>Click me!</button>
+    </div>
+  );
+};
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Route path='/' exact component={PageOne} />
+      <Route path='/pagetwo' component={PageTwo} />
+    </BrowserRouter>
+  );
 };
 export default App;
