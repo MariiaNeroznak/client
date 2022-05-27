29b90399db8ea53c9ce1d25b199b64fa7a12ef23
Mariia Neroznak <mariia.neroznak@visma.com>
2022-05-27 10:33:25 +0300

    Add sing in button

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -24,10 +24,10 @@ class GoogleAuth extends React.Component {
     this.setState({ isSignedIn: this.auth.isSignedIn.get() });
   };
 
-  onSignIn = () => {
+  onSignInClick = () => {
     this.auth.signIn();
   };
-  onSignOut = () => {
+  onSignOutClick = () => {
     this.auth.signOut();
   };
 
@@ -35,13 +35,13 @@ class GoogleAuth extends React.Component {
     if (this.state.isSignedIn === null) return null;
     else if (this.state.isSignedIn)
       return (
-        <button className='ui red google button' onClick={this.onSignOut}>
+        <button className='ui red google button' onClick={this.onSignOutClick}>
           <i className='google icon'></i> Sign Out
         </button>
       );
     else
       return (
-        <button className='ui red google button' onClick={this.onSignIn}>
+        <button className='ui red google button' onClick={this.onSignInClick}>
           <i className='google icon'></i> Sign In with Google
         </button>
       );
